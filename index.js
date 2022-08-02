ff51df07c85ada6c53db73828931c7a38d49eb6f
Kam <Kamwescott@gmail.com>
2022-08-02 13:26:19 -0700

    starting to add html

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import express from 'express';
+import express, { response } from 'express';
 import pg from 'pg';
+import fs from 'fs'
 import dotenv from "dotenv"; 
 
-const app = express()
+const app = express(); 
 const {Pool} = pg;
+
 let PORT = process.env.PORT
 if(PORT == null || PORT =='') PORT = 8000; 
 
@@ -14,9 +16,19 @@ if(PORT == null || PORT =='') PORT = 8000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send('hello')
-})
+
+
+app.get('/', (req, res) => {
+    res.writeHead(200, {
+        'Content-Type' : 'text/html'
+    })
+   
+    fs.readFile("index.html", (err, data) => {
+        res.write(data)
+    })
+
+});
+  
 
 app.use((err, req, res, next) => {
     res.sendStatus(500)
